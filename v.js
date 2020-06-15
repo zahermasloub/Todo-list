8513d5f53a4e131054407a085fcd2a576d7d40ce
zaher <crc.qa2222@gmail.com>
2020-06-15 22:29:51 +0300

    a

--- v.js
+++ v.js
@@ -1,76 +1,62 @@
-// const inbe = document.getElementById("inbt");
-
+const inbe = document.getElementById("inbt");
 
 const btn = document.querySelector(".btn3");
 
 const ul = document.getElementById("rootmap");
 
-// const inputs = document.querySelector(".inbt");
-
-
-
-
-
+const inputs = document.querySelector(".inbt");
 
 window.onload = function () {
-    inputs.focus();
+  inputs.focus();
 };
 
 btn.onclick = function () {
-    if (inputs.value === "") {
+  if (inputs.value === "") {
+    console.log("kkkkkkkkkk");
+  } else {
+    const li = document.createElement("li");
 
-        console.log("kkkkkkkkkk");
-    } else {
-        const li = document.createElement("li");
+    li.className = "item";
 
-        li.className = "item";
+    const links = document.createElement("a");
+    links.className = "spanbtn";
 
-        const links = document.createElement("a");
-        links.className = "spanbtn";
+    const trr = document.createTextNode("X");
 
-        const trr = document.createTextNode("X");
+    const texttext = document.createTextNode(inputs.value);
 
-
-
-        const texttext = document.createTextNode(inputs.value);
-
-        links.appendChild(trr);
-        li.appendChild(texttext);
-        li.appendChild(links);
-        ul.appendChild(li);
-        inputs.value = "";
-
-    }
+    links.appendChild(trr);
+    li.appendChild(texttext);
+    li.appendChild(links);
+    ul.appendChild(li);
+    inputs.value = "";
+  }
 };
 
-document.addEventListener('click', function (e) {
-    if (e.target.className === "spanbtn") {
-        // console.log('ok ok ok');
+document.addEventListener("click", function (e) {
+  if (e.target.className === "spanbtn") {
+    // console.log('ok ok ok');
 
-        e.target.parentElement.remove();
-
-    }
+    e.target.parentElement.remove();
+  }
 });
 
-
-btn.addEventListener("click", goo);
-
-
+const inputs2 = document.querySelector(".btn2");
+inputs2.addEventListener("click", goo);
 
 function goo() {
-    const inputs = document.querySelector(".inbt");
+  const inputs = document.querySelector(".inbt");
 
-    let = tasks;
+  let = tasks;
 
-    if (localStorage.setItem(tasks) === null) {
-        tasks = [];
-    } else {
-        tasks = JSON.parse(localStorage.getItem(tasks));
-    }
+  if (localStorage.setItem(tasks) === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem(tasks));
+  }
 
-    tasks.push(inputs.value);
+  tasks.push(inputs.value);
 
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    alert("ccccc");
-
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  alert("ccccc");
 }
