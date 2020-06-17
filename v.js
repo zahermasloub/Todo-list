bc286292c7e29776806c16e7853a9bd2ba65a4fd
zaher <crc.qa2222@gmail.com>
2020-06-17 13:17:57 +0300

    filtering 2

--- v.js
+++ v.js
@@ -91,5 +91,16 @@ const filterdiv = document.querySelector(".filterfilter-conten");
 filterinput.addEventListener("keyup", filtterTasks);
 
 function filtterTasks(e) {
-  const text = e.target.value;
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll(".item").forEach(function (task) {
+    const itemm = task.firstChild.textContent;
+    if (itemm.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+
+    } else {
+      task.style.display = 'none';
+    }
+  });
+
+  console.log(text);
 }
